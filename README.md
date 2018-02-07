d78e21c16182b435e1d2b036c9699ac4ed7d9c8e
mrmalhotra <deepakmalhotra2070@gmail.com>
2018-02-08 01:39:58 +0530

    added readme and other object functions

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 ## Meaningful String
 **Meaningful String** is an npm package which generates a random meaningful string.
+
+###Getting Started
+Here is how you can install this package globally using npm
+
+####Installation
+using npm
+```
+$ npm install meaningful-string -g
+```
+
+In Node.js:
+
+```
+//Load the full package
+const meaningfulString = require('meaningful-string');
+
+//Generate random string
+const generate = new meaningfulString();
+
+console.log('Random String: ',generate.random());
+```
+
+######Result
+``Random String HJ7H9Lkt7hFdS7mJY48NnmHH6hDdg76lIfaD4MhU6Y``
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const upperCaseAlphabats = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const smallAlphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const upperAlphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123409876';
 const mixedChars = 'NOPQRmDTBCMljLSIJkno3p051F787643G8EArKqH';
+
 function generateString(first, last) {
 this.first = first;
 this.last = last;
@@ -16,15 +17,50 @@ generateString.prototype.animal = function () {
 }
 
 generateString.prototype.random = function (object) {
-
     let string = '';
-    let lengthShouldBe = mixedChars.length;
-    if(object && object.max)
-        lengthShouldBe = parseInt(object.max);
+    let referralString = mixedChars;
+    let lengthShouldBeAtlast = mixedChars.length;
+    let lengthShouldBeAtleast = 4;
+    let lengthShouldBe = lengthShouldBeAtlast;
 
-        for(let i =0;i< lengthShouldBe;i++){
-           let val =  Math.floor(Math.random() * mixedChars.length-1) +1;
-            string += mixedChars[val] ;
-        }
-        return string;
+    // let capsWithNumbers = object.capsWithNumbers;
+    // let smallWithNumbers = object.smallWithNumbers;;
+    // let onlyNumbers = object.onlyNumbers;;
+    // let onlySmallCase = object.onlySmallCase;;
+    // let onlyCapsCase = object.onlyCapsCase;;
+
+    if(object && object.allCaps)
+        referralString = upperCaseAlphabats;
+
+    if(object && object.allSmall)
+        referralString = smallCaseAlphabats;
+
+    if(object && object.capsWithNumbers)
+        referralString = upperAlphanumeric;
+
+    if(object && object.smallWithNumbers)
+        referralString = smallAlphanumeric;
+
+    if(object && object.onlyNumbers)
+        referralString = number;
+
+
+
+    if (object && object.max)
+        lengthShouldBeAtlast = parseInt(object.max);
+
+    if (object && object.max)
+        lengthShouldBeAtleast = parseInt(object.min);
+
+    if (object && object.max && object.min){
+       let minimum = lengthShouldBeAtlast - lengthShouldBeAtleast;
+       let maximum = lengthShouldBeAtlast - minimum;
+        lengthShouldBe = Math.ceil(Math.random() * minimum) + maximum ;
+    }
+
+    for (let i = 0; i < lengthShouldBe; i++) {
+        let val = Math.floor(Math.random() * referralString.length - 1) + 1;
+        string += referralString[val];
+    }
+    return string;
 };
